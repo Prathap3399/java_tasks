bfdae4d94802af1ba2b2f3420582b0ae2a7cde0b
Prathap3399 <60866728+Prathap3399@users.noreply.github.com>
2025-03-23 14:22:48 +0530

    Update GCD.java

--- java/GCD.java
+++ java/GCD.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class GCD {
     static int gcd(int a, int b){
-        if (b!=0){
+        if (b==0){
             return a;           
         }else{
             return gcd(b,a%b);
@@ -10,8 +10,16 @@ public class GCD {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
            int a = sc.nextInt();
            int b = sc.nextInt();
+
         System.out.println("GCD is : "+gcd(a, b));
+        sc.close();
     }
 }
+
+// Steps:
+//  Recursive : the function calls until become 0.
+//  Base Condition when b==0;
+//  Time complexity: O(log(min(a, b)))
